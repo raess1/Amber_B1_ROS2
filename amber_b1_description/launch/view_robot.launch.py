a27f9e96dc5d7b7569dc75aad5492d25fb2a5fcd
Robin Fröjd <robinae@hotmail.se>
2022-03-18 14:16:43 +0100

    adding first files

---
+++ amber_b1_description/launch/view_robot.launch.py
@@ -0,0 +1,73 @@
+# Copyright 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+
+
+    # get urdf path
+
+    #robot_description_path = PathJoinSubstitution(
+    #            [FindPackageShare(description_package), "urdf", description_file]
+    #        )
+    robot_description_path = os.path.join(
+                            get_package_share_directory("amber_b1_description"),
+                            'urdf/amber_b1.urdf')
+
+    print(robot_description_path)
+    robot_description_content = open(robot_description_path).read()
+    
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+         [FindPackageShare("amber_b1_description"), "rviz", "view2.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
