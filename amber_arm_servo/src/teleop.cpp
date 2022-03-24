7323c73cbf185f7e080e91ae38c8e082c8eca631
Robin Fröjd <robinae@hotmail.se>
2022-03-24 11:27:28 +0100

    Update teleop.cpp
    Adding individual joint control for joint1, joint2, joint6 and joint7

--- amber_arm_servo/src/teleop.cpp
+++ amber_arm_servo/src/teleop.cpp
@@ -98,9 +98,27 @@ std::map<Button, double> BUTTON_DEFAULTS;
  * @return return true if you want to publish a Twist, false if you want to publish a JointJog
  */
 bool convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& buttons,
-                     std::unique_ptr<geometry_msgs::msg::TwistStamped>& twist)
+                     std::unique_ptr<geometry_msgs::msg::TwistStamped>& twist,
+                     std::unique_ptr<control_msgs::msg::JointJog>& joint)
 {
+  // Give joint jogging priority because it is only buttons
+  // If any joint jog command is requested, we are only publishing joint commands
+  if (buttons[A] || buttons[B] || buttons[X] || buttons[Y] || axes[D_PAD_X] || axes[D_PAD_Y])
+  {
+    // Map the D_PAD to the proximal joints
+    joint->joint_names.push_back("joint1");
+    joint->velocities.push_back(axes[D_PAD_X]);
+    joint->joint_names.push_back("joint2");
+    joint->velocities.push_back(axes[D_PAD_Y]);
 
+    // Map the diamond to the distal joints
+    joint->joint_names.push_back("joint7");
+    joint->velocities.push_back(buttons[B] - buttons[X]);
+    joint->joint_names.push_back("joint6");
+    joint->velocities.push_back(buttons[Y] - buttons[A]);
+    return false;
+  }
+  
   // The bread and butter: map buttons to twist commands
   twist->twist.linear.z = axes[RIGHT_STICK_Y];
   twist->twist.linear.y = axes[RIGHT_STICK_X];
@@ -161,21 +179,28 @@ public:
   {
     // Create the messages we might publish
     auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+    auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
+
 
     // This call updates the frame for twist commands
     updateCmdFrame(frame_to_publish_, msg->buttons);
 
-    // Convert the joystick message to Twist or JointJog and publish
-    if (convertJoyToCmd(msg->axes, msg->buttons, twist_msg))
+        // Convert the joystick message to Twist or JointJog and publish
+    if (convertJoyToCmd(msg->axes, msg->buttons, twist_msg, joint_msg))
     {
       // publish the TwistStamped
       twist_msg->header.frame_id = frame_to_publish_;
       twist_msg->header.stamp = this->now();
       twist_pub_->publish(std::move(twist_msg));
     }
-
+    else
+    {
+      // publish the JointJog
+      joint_msg->header.stamp = this->now();
+      joint_msg->header.frame_id = "three_Link";
+      joint_pub_->publish(std::move(joint_msg));
+    }
   }
-
 private:
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
