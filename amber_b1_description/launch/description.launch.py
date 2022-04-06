aa4a3d07f1d1742b70b2d4bbb45c88ac0f5338e1
Robin Fröjd <robinae@hotmail.se>
2022-04-06 11:51:29 +0200

    adding
    adding

---
+++ amber_b1_description/launch/description.launch.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    urdf_path = PathJoinSubstitution(
+        [FindPackageShare("amber_b1_description"), "urdf", "amber_b1.urdf.xacro"]
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('amber_b1_description'), 'rviz', 'view.rviz']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='urdf', 
+            default_value=urdf_path,
+            description='URDF path'
+        ),
+        
+        DeclareLaunchArgument(
+            name='publish_joints', 
+            default_value='true',
+            description='Launch joint_states_publisher'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='false',
+            description='Run rviz'
+        ),
+
+        DeclareLaunchArgument(
+            name='use_sim_time', 
+            default_value='false',
+            description='Use simulation time'
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            condition=IfCondition(LaunchConfiguration("publish_joints")),
+            parameters=[
+                {'use_sim_time': LaunchConfiguration('use_sim_time')}
+            ]
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[
+                {
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'robot_description': Command([
+                        'xacro ', 
+                        LaunchConfiguration('urdf'), 
+                        ' use_sim_time:=', 
+                        LaunchConfiguration('use_sim_time')
+                    ])
+                }
+            ]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
